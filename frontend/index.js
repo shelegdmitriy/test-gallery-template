8a505e8cf4be55795857185b14899c1a8aa9f9fe
Dmitriy <sheleg.dmitriy@gmail.com>
2022-11-21 15:59:57 +0200

    Update for preview

--- frontend/index.js
+++ frontend/index.js
@@ -7,17 +7,18 @@ import App from './App';
 import { HelloNEAR } from './near-interface';
 import { Wallet } from './near-wallet';
 
+const CONTRACT_NAME = process.env.CONTRACT_NAME || 'near-blank-project.sheleg3.testnet';
+
 // When creating the wallet you can optionally ask to create an access key
 // Having the key enables to call non-payable methods without interrupting the user to sign
-const wallet = new Wallet({ createAccessKeyFor: process.env.CONTRACT_NAME });
+const wallet = new Wallet({ createAccessKeyFor: CONTRACT_NAME });
 
 // Abstract the logic of interacting with the contract to simplify your flow
-const helloNEAR = new HelloNEAR({ contractId: process.env.CONTRACT_NAME, walletToUse: wallet });
+const helloNEAR = new HelloNEAR({ contractId: CONTRACT_NAME, walletToUse: wallet });
 
 // Setup on page load
 window.onload = async () => {
   const isSignedIn = await wallet.startUp();
-
   ReactDOM.render(
     <App isSignedIn={isSignedIn} helloNEAR={helloNEAR} wallet={wallet} />,
     document.getElementById('root'),
